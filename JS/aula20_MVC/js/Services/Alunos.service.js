5da8009c0170ace857e26f9926cfac1946042d9c
André Carvalho <andre.vrlcarvalho@hotmail.com>
2021-08-30 19:22:05 -0300

    Estudo MVC

--- JS/aula20_MVC/js/Services/Alunos.service.js
+++ JS/aula20_MVC/js/Services/Alunos.service.js
@@ -1,6 +1,7 @@
 class AlunosService{
     constructor(){
         this.alunos = []
+        this.updateListAlunosFromLocalStorage()
     }
 
     add(aluno){
@@ -8,9 +9,29 @@ class AlunosService{
             throw TypeError("aluno must be a insteance of AlunoModel")
         }
         this.alunos.push(aluno)
+        this.updateLocalStorage()
     }
 
     edit(aluno){
         return aluno
+    }
+
+    serchById(id){
+        return this.alunos.find(aluno => aluno._id === id)//retorna o aluno que tem o id passado por parametro igual sua propriedade id
+    }
+
+    updateLocalStorage(){//função criada para armazenar os alunos criados na página no local storage 
+        const alunos = JSON.stringify(this.alunos)
+        localStorage.setItem("alunos",alunos)//somente itens de tipo string podem ser armazenados
+    }
+
+    updateListAlunosFromLocalStorage(){//função criada para atualizar a página com os alunos armazenados no local storage
+        const local = localStorage.getItem("alunos")
+        if(local){
+            const alunos = JSON.parse(local)
+            alunos.forEach(aluno => {
+                this.add(new AlunoModel(aluno))
+            })
+        }
     }
 }
--- JS/aula20_MVC/js/Views/Alunos.view.js
+++ JS/aula20_MVC/js/Views/Alunos.view.js
@@ -24,14 +24,29 @@ class AlunosView{
             alunos.forEach(aluno =>{//cria um loop para a constante alunos passando o objeto como parametro
                 const htmlBody = document.createElement("tr")//adiciona um elemento html de tag tr
                 let htmlMedias = `<td>${aluno.nome}</td>`//concatena o nome do aluno com tag td
-                this.materias.forEach(materia => {//forma uma array com os nomes das matérias do aluno, e cria um forEach com esses nomes das propriedades por parametro
-                    htmlMedias += `<td>
-                    ${aluno.media[materia] !== undefined ? 
-                        aluno.media[materia] :
-                        `<a href="edit.html?id=${aluno._id}">Incluir nota</a>`
+                let encontrado = false
+
+                this.materias.forEach(materia => {//loop para cada matéria passando matéria como parametro
+                    if(materia in aluno.notas){//caso o aluno tenha nota na matéria
+                        encontrado = true
                     }
-                    </td>`//concatena a tag td a média do aluno em cada matéria
                 })
+                if (encontrado){//caso tenha nota em ao menos 1 matéria incluir link por nota
+                    this.materias.forEach(materia => {//forma uma array com os nomes das matérias do aluno, e cria um forEach com esses nomes das propriedades por parametro
+                        htmlMedias += `<td>
+                        ${aluno.media[materia] !== undefined ? 
+                            aluno.media[materia] :
+                            `<a href="edit.html?id=${aluno._id}">Incluir nota</a>`
+                        }
+                        </td>`//concatena a tag td a média do aluno em cada matéria
+                    })
+                }   else {//caso n tenha ao menos uma nota, incluir um link para todas
+                    htmlMedias += `<td colspan ="${this.materias.length}">
+                        <a href="edit.html?id=${aluno._id}">Incluir notas</a>
+                    </td>`
+                }
+
+                
                 htmlBody.innerHTML = htmlMedias//concatena as tag td de média com a tag td de nome
                 this.tableBody.appendChild(htmlBody)//adiciona essas tag tr e td ao body como filho
             })
