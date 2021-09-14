322e78d06b278042baa1d44e50d26885079916ee
André Carvalho <andre.vrlcarvalho@hotmail.com>
2021-09-14 15:04:28 -0300

    JS 14-09

---
+++ JS/aula22_Ajax/todolist/js/createXMLHttpRequest.js
@@ -0,0 +1,40 @@
+export function createXMLHttpRequest(method, url, cb, data = null) {
+    const xhr = new XMLHttpRequest()
+
+    xhr.open(method, url)
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")//configura que esta trabalhando com json
+    xhr.send(data)
+
+    xhr.onreadystatechange = verificaAjax
+
+    function verificaAjax() {
+        // console.log(xhr.readyState)
+        // //0 - Conexão não iniciada
+        // //1 - resquest configurada
+        // //2 - request enviada
+        // //3 - em processamento
+        // //4 - resposta recebida(Completa: sucesso ou falha)
+        // console.log(xhr.status)
+        // //200 - ok
+        // //304 em cache
+        // //404 error
+        // console.log(xhr.response)
+        // console.log(xhr.responseText)
+        if (xhr.readyState === 4) {
+            if (xhr.status < 400) {
+                // console.log(xhr.responseText)//retorna um array de objetos em formato de string
+                const json = JSON.parse(xhr.responseText)//transforma a string em um array
+
+                if (typeof cb === "function") {
+                    cb(json)
+                }
+            } else if (typeof cb === "function") {
+                cb({
+                    error: true,
+                    status: xhr.status,
+                    message: "algo deu errado com a conexão"
+                })
+            }
+        }
+    }
+}
--- JS/aula22_Ajax/todolist/js/todolist.js
+++ JS/aula22_Ajax/todolist/js/todolist.js
@@ -1,67 +1,26 @@
-; (function () {
-    "use strict"
+import { Task } from "./Model/Task.model.js"
+import { createXMLHttpRequest } from "./createXMLHttpRequest.js"
+
+// const url = "https://jsonplaceholder.typicode.com/users/1/todos/"
+const urlUsers = "http://localhost:3000/users"
+const urlTasks = "http://localhost:3000/tasks"
+
+const userId = 2
+
+createXMLHttpRequest("GET", `${urlUsers}/${userId}/tasks`, init)
 
 
-
-    function Task(name, completed, createdAt, updatedAt) {
-        // crie uma funcao construtora chamada Task. 
-        // essa funcao recebe por parametro obrigatório o nome da tarefa
-        // também recebe tres parametros opcionais (completed, createdAt, updatedAt)
-        // o objeto retornado por essa funcao deve ter quatro propriedades:
-        //  - name - string - obrigatório, 
-        //  - completed - boolean - opcional, false é o default, 
-        //  - createdAt - timestamp - opcional, timestamp atual é o valor default)  Date.now()
-        //  - updatedAt - timestamp - opcional, null é o valor default
-        // o objeto retornado por essa funcao deve ter um método chamado toggleDone, que deve inverter o boolean completed
-
-        if (!name) {
-            throw new Error("Task need a required parameter: name")
-        }
-        let _name = name
-        // this.name = name
-        this.completed = completed || false
-        this.createdAt = createdAt || Date.now()
-        this.updatedAt = updatedAt || null
-        this.toggleDone = function () {
-            this.completed = !this.completed
-        }
-        this.getName = () => _name
-        this.setName = function (newName) {
-            _name = newName
-            this.updatedAt = Date.now()
-            console.log("------")
-            console.log(this)
-        }
-
-
-    }
-
-    let arrTasks = [
-        {
-            name: "task 1",
-            completed: true,
-            createdAt: 1592667375012,
-            updatedAt: null
-        },
-        {
-            name: "task 2",
-            createdAt: 1581667345723,
-            updatedAt: 1592667325018
-        },
-        {
-            name: "task 3",
-            completed: true,
-            createdAt: 1592667355018,
-            updatedAt: 1593677457010
-        }
-    ]
-
+function init(arrTasks) {
 
     // a partir de um array de objetos literais, crie um array contendo instancias de Tasks. 
     // Essa array deve chamar arrInstancesTasks
+    if (arrTasks.error) {
+        return
+    }
+
     const arrInstancesTasks = arrTasks.map(task => {
-        const { name, completed, createdAt, updatedAt } = task
-        return new Task(name, completed, createdAt, updatedAt)
+        const { title, completed, createdAt, updatedAt } = task
+        return new Task(title, completed, createdAt, updatedAt)
     })
 
 
@@ -91,7 +50,7 @@
         li.appendChild(checkButton)
 
         p.className = "task-name"
-        p.textContent = obj.getName()
+        p.textContent = obj.getTitle()
         li.appendChild(p)
 
         editButton.className = "fas fa-edit"
@@ -104,7 +63,7 @@
         const inputEdit = document.createElement("input")
         inputEdit.setAttribute("type", "text")
         inputEdit.className = "editInput"
-        inputEdit.value = obj.getName()
+        inputEdit.value = obj.getTitle()
 
         containerEdit.appendChild(inputEdit)
         const containerEditButton = document.createElement("button")
@@ -136,10 +95,15 @@
         });
     }
 
-    function addTask(taskName) {
+    function addTask(title) {
         // adicione uma nova instancia de Task
-        arrInstancesTasks.push(new Task(taskName))
-        renderTasks()
+        const cb = function ({ title }) {
+            arrInstancesTasks.push(new Task(title))
+            renderTasks()
+        }
+        const taskString = JSON.stringify({ title, userId })
+        createXMLHttpRequest("POST", urlTasks, cb, taskString)
+
 
     }
 
@@ -178,7 +142,7 @@
             },
             containerCancelButton: function () {
                 currentLi.querySelector(".editContainer").removeAttribute("style")
-                currentLi.querySelector(".editInput").value = arrInstancesTasks[currentLiIndex].getName()
+                currentLi.querySelector(".editInput").value = arrInstancesTasks[currentLiIndex].getTitle()
             },
             checkButton: function () {
 
@@ -207,4 +171,5 @@
 
     renderTasks()
 
-})();
+
+}
