621207214c1a2a3620b27bc93bb0c0293c1e90ca
André Carvalho <andre.vrlcarvalho@hotmail.com>
2021-10-11 22:17:45 -0300

    Aula storion 11-10

---
+++ JS/storionPHP/aula11-10/biblioteca.php
@@ -0,0 +1,48 @@
+<html><head>
+      <title>Resultados da consulta</title>
+   <style type = "text/css">
+         body  { font-family: arial, sans-serif;
+                 background-color: #F0E68C } 
+         table { background-color: #ADD8E6 }
+         td    { padding-top: 2px;
+                 padding-bottom: 2px;
+                 padding-left: 4px;
+                 padding-right: 4px;
+                 border-width: 1px;
+                 border-style: inset }
+      </style></head><body>
+      <?php
+         extract( $_POST );
+         
+		$servidor = "localhost";
+		$usuario = "root";
+		$senha = "1234";
+		$bancodados = "biblioteca";
+		$conexao = new mysqli($servidor, $usuario, $senha, $bancodados);
+		if ($conexao->connect_error) {
+			die("Conexão falhou: " . $conexao->connect_error);
+} 
+
+		$consulta = "SELECT " . $selecionar . " FROM livro";
+		$resultado = $conexao->query($consulta);
+         
+      ?><!-- fim do  PHP script -->
+      <h3>Resultados da pesquisa</h3>
+      <table>
+         <?php
+            print( "<tr>" );
+			if ($resultado = $conexao -> query($consulta)) {
+			
+            print( "$selecionar" );
+			while ($acesso = $resultado -> fetch_row()) {
+				printf ("<td> %s \n", $acesso[0]);
+				print( "</tr>" );
+				
+			}
+				$resultado -> free_result();
+			}
+		$conexao->close();
+		?><!-- fim do PHP script -->
+      </table>
+      <br />Sua pesquisa para Livros
+      </body></html>
