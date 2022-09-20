dd53085bc8c51307dd33901bc066c438b6ca04e5
André Carvalho <andre.vrlcarvalho@hotmail.com>
2022-09-20 11:44:59 -0300

    Resolução do professor

--- JS/aula26_TypeScript/in2cm-starter/src/js/index.ts
+++ JS/aula26_TypeScript/in2cm-starter/src/js/index.ts
@@ -1,6 +1,31 @@
 const inches = document.getElementById("inches") as HTMLInputElement
 const centim = document.getElementById("centimeter") as HTMLInputElement
-inches.addEventListener("input", e =>  {
-    centim.value = (inches.value as unknown as number * 2.54) as unknown as string
-})
+type FnListener = (this: HTMLInputElement, e: Event) => void
+
+function calculateCm(inche: number): number {
+    return inche * 2.54
+}
+
+const convertToCm: FnListener = function (e) {
+    if (this.value != "") {
+        centim.value = (calculateCm(parseFloat(this.value))).toString()
+    } else {
+        centim.value = ""
+    }
+}
+
+function calculateInche(cm: number): number {
+    return cm / 2.54
+}
+
+const convertToInche: FnListener = function (e) {
+    if (this.value != "") {
+        inches.value = (calculateInche(parseFloat(this.value))).toString()
+    } else {
+        inches.value = ""
+    }
+}
+
+inches.addEventListener("input", convertToCm)
+centim.addEventListener("input", convertToInche)
 
